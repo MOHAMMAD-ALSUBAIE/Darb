3ed17cc9b460f42f39703e6cbdf06a1bb7f50991
MOHAMMAD-ALSUBAIE <alsubaiemoh672@gmail.com>
2023-12-07 08:30:17 +0300

    fiexed

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 import Route from "./Routers/itinerary.route";
 import RouterInstance from './Routers/authentication.route'
 import userRoute from './Routers/user.route'
-import bodyParser from "body-parser";
 import cors from 'cors'
 import expressSession from 'express-session';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import  { PrismaClient } from '@prisma/client';
-import cookieParser from "cookie-parser"
-const port=3000
+import fs from 'fs';
+
+dotenv.config({ path: "./.env" });
+const port=process.env.port||3000
 const app= express()
 declare module "express-session" {
   interface SessionData {
@@ -25,7 +26,6 @@ app.use(cors({
   origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
   credentials: true,
-//  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }))
 
 
@@ -34,7 +34,7 @@ app.use(cors({
 
 //@ts-ignore
 
-//app.use(cookieParser())
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -42,7 +42,7 @@ app.use(
         cookie: {
             maxAge: 60 * 60 * 1000, // ms
         },
-        secret: "a santa at nasa",
+        secret: process.env.secretKey,
         resave: false,
         saveUninitialized: false,
         store: new PrismaSessionStore(new PrismaClient(), {
@@ -63,3 +63,6 @@ app.use("/user/",RouterInstance,userRoute)
 app.listen(port,()=>{
   console.log(`server is running on port ${port}`)  
 })
+
+
+
